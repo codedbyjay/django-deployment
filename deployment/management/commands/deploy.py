b55fcb01d8d81420af01584b21d6a0aa20b90b32
jaywhy13 <jeanmark.wright@gmail.com>
2015-06-04 12:36:50 -0500

    Stop hard coding server info and use the config file.

--- deployment/management/commands/deploy.py
+++ deployment/management/commands/deploy.py
@@ -1,15 +1,23 @@
+from optparse import make_option
+
 from django.core.management.base import BaseCommand
 
 from fabric.api import env, execute
 
 from deployment.fabfile import deploy
+from deployment import setup_fabric_environment
 
 class Command(BaseCommand):
     help = "Deploys the project to the server"
 
+    option_list = BaseCommand.option_list + (
+        make_option('--server',
+            dest='server',
+            default=None,
+            help='The server to deploy to'),
+        )
+
     def handle(self, *args, **options):
-        env.hosts = ["uem.monagis.com"]
-        env.username = "root"
-        env.password = "mnwp4$$"
+        setup_fabric_environment(options["server"])
         execute(deploy)
 
