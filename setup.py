df726a7791b36f68c3485ffe3c603c5f2298b766
jaywhy13 <jeanmark.wright@gmail.com>
2015-06-12 15:24:11 -0500

    Bump to v0.1.1 and included Manifest

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+current_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(current_dir, 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -9,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-deployment',
-    version='0.1',
+    version='0.1.1',
     packages=['deployment'],
     include_package_data=True,
     license='GPL v3 License',  # example license
@@ -32,5 +33,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    install_requires=[ req for req in open("%s/requirements.txt" % os.path.dirname(__file__),"r").read().splitlines() if req.strip() and not req.startswith("#")]
+    install_requires=[ req for req in open("%s/requirements.txt" % current_dir,"r").read().splitlines() if req.strip() and not req.startswith("#")]
 )
