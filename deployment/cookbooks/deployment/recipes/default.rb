0457f008dff2ae440210b05d51cb34e864fb4950
Jean-Mark Wright <jeanmark.wright@gmail.com>
2015-06-18 11:16:30 -0500

    added main_app_name, server_name and gunicorn port. Upstart for gunicorn and nginx templates.
    Also stopped the main service and started it after install

--- deployment/cookbooks/deployment/recipes/default.rb
+++ deployment/cookbooks/deployment/recipes/default.rb
@@ -12,6 +12,16 @@ project_dir = node["project"]["deployment"]["project_dir"]
 project_name = node["project"]["deployment"]["project_name"]
 username = node["project"]["deployment"]["username"]
 main_app_dir = node["project"]["deployment"]["main_app_dir"]
+main_app_name = node["project"]["deployment"]["main_app_name"]
+server_name = node["project"]["deployment"]["server_name"]
+gunicorn_port = 8000
+
+# Stop service if it exists
+service project_name do
+    action :stop
+    ignore_failure true
+end
+
 
 
 # Install our packages
@@ -76,3 +86,81 @@ for initial_data in initial_datas do
         EOH
     end
 end
+
+# Setup Gunicorn script
+template "#{project_dir}/bin/run_gunicorn.sh" do
+    source "run_gunicorn.sh.erb"
+    action :create
+    mode "555"
+    variables({
+        :project_dir => project_dir,
+        :username => username,
+        :project_name => project_name,
+        :main_app_name => main_app_name
+        })
+end 
+
+# Setup upstart for gunicorn
+template "/etc/init/#{project_name}.conf" do
+    source "upstart_gunicorn.conf.erb"
+    action :create
+    variables({
+        :project_dir => project_dir
+        })
+end
+
+link "/etc/init.d/#{project_name}" do
+  to "/lib/init/upstart-job"
+end
+
+# Setup nginx conf
+service "nginx" do
+    action :stop
+    ignore_failure true
+end
+
+# remove some default config files
+config_files = ["/etc/nginx/sites-available/default", 
+    "/etc/nginx/sites-enabled/default"]
+
+for config_file in config_files do 
+    file config_file do
+        action :delete
+        ignore_failure 
+    end
+end
+
+template "/etc/nginx/sites-available/#{project_name}" do
+    source "nginx.conf.erb"
+    action :create
+    variables({
+        :project_dir => project_dir,
+        :server_name => server_name,
+        :gunicorn_port => gunicorn_port
+        })
+end
+
+link "/etc/nginx/sites-enabled/#{project_name}" do
+  to "/etc/nginx/sites-available/#{project_name}"
+end
+
+# Restart postgres
+service "postgresql" do
+    action :restart
+    ignore_failure true
+end
+
+# Create a logs dir
+directory "#{project_dir}/logs" do
+    action :create
+end
+
+service "nginx" do
+    action :start
+end
+
+# Stop service if it exists
+service project_name do
+    action :start
+end
+
