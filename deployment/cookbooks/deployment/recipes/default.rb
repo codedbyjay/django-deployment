a80e48a674d9561222494e8dfcffe94421665f2c
jaywhy13 <jeanmark.wright@gmail.com>
2015-07-02 15:18:08 -0500

    Updated pip command, removed the options that are no longer available. Stop the service for the project if one exists at the start. Added some more settings at the top. Corrected site_url lookup location. Added loaddata to command. Added template for setting up Gunicorn, nginx and celery

--- deployment/cookbooks/deployment/recipes/default.rb
+++ deployment/cookbooks/deployment/recipes/default.rb
@@ -12,6 +12,16 @@ project_dir = node["project"]["deployment"]["project_dir"]
 project_name = node["project"]["deployment"]["project_name"]
 username = node["project"]["deployment"]["username"]
 main_app_dir = node["project"]["deployment"]["main_app_dir"]
+main_app_name = node["project"]["deployment"]["main_app_name"]
+server_name = node["project"]["deployment"]["server_name"]
+gunicorn_port = 8000
+
+# Stop service if it exists
+service project_name do
+    action :stop
+    ignore_failure true
+end
+
 
 
 # Install our packages
@@ -34,7 +44,7 @@ python_pip "--upgrade pip" do
 end
 
 if File.exist?("#{project_dir}/requirements.txt")
-    python_pip "--exists-action=w -r #{project_dir}/requirements.txt" do
+    python_pip "-r #{project_dir}/requirements.txt" do
         virtualenv project_dir
     end
 end
@@ -50,7 +60,7 @@ template "#{main_app_dir}/local_settings.py" do
         :database_host => node["project"]["database"]["host"],
         :database_port => node["project"]["database"]["port"],
         :database_engine => node["project"]["database"]["engine"],
-        :site_url => node["project"]["database"]["site_url"],
+        :site_url => node["project"]["deployment"]["site_url"],
     })
 end
 
@@ -72,7 +82,111 @@ for initial_data in initial_datas do
         code <<-EOH
         cd #{project_dir}
         source bin/activate
-        python manage.py #{initial_data}
+        python manage.py loaddata #{initial_data}
         EOH
     end
 end
+
+# Setup Gunicorn script
+template "#{project_dir}/bin/run_gunicorn.sh" do
+    source "run_gunicorn.sh.erb"
+    action :create
+    mode "555"
+    variables({
+        :project_dir => project_dir,
+        :username => username,
+        :project_name => project_name,
+        :main_app_name => main_app_name
+        })
+end 
+
+# Setup upstart for gunicorn
+template "/etc/init/#{project_name}.conf" do
+    source "upstart_gunicorn.conf.erb"
+    action :create
+    variables({
+        :project_dir => project_dir
+        })
+end
+
+link "/etc/init.d/#{project_name}" do
+  to "/lib/init/upstart-job"
+end
+
+# Setup celery script
+template "#{project_dir}/bin/run_celery.sh" do
+    source "run_celery.sh.erb"
+    action :create
+    mode "555"
+    variables({
+        :project_dir => project_dir,
+        :username => username
+        })
+end 
+
+# Setup upstart for celery
+template "/etc/init/#{project_name}-celery.conf" do
+    source "upstart-#{project_name}-celery.conf.erb"
+    action :create
+    variables({
+        :project_dir => project_dir,
+        :project_name => project_name,
+        })
+end
+
+link "/etc/init.d/#{project_name}-celery" do
+  to "/lib/init/upstart-job"
+end
+
+
+# Setup nginx conf
+service "nginx" do
+    action :stop
+    ignore_failure true
+end
+
+# remove some default config files
+config_files = ["/etc/nginx/sites-available/default", 
+    "/etc/nginx/sites-enabled/default"]
+
+for config_file in config_files do 
+    file config_file do
+        action :delete
+        ignore_failure 
+    end
+end
+
+template "/etc/nginx/sites-available/#{project_name}" do
+    source "nginx.conf.erb"
+    action :create
+    variables({
+        :project_dir => project_dir,
+        :server_name => server_name,
+        :gunicorn_port => gunicorn_port
+        })
+end
+
+link "/etc/nginx/sites-enabled/#{project_name}" do
+  to "/etc/nginx/sites-available/#{project_name}"
+end
+
+# Restart postgres
+service "postgresql" do
+    action :restart
+    ignore_failure true
+end
+
+# Create a logs dir
+directory "#{project_dir}/logs" do
+    action :create
+end
+
+service "nginx" do
+    action :start
+end
+
+# Stop service if it exists
+service project_name do
+    action :start
+end
+
